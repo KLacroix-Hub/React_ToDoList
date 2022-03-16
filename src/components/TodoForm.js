092265a1fa598fc6c27c4fd14fb31a3ffec70a45
KLacroix-Hub <kevin.lacroix@heaven.fr>
2022-03-16 23:06:51 +0100

    Update fonctionnalités toDoList

---
+++ src/components/TodoForm.js
@@ -0,0 +1,87 @@
+import React, {useState, useEffect, useRef} from 'react';
+
+function TodoForm(props) {
+    const [text, setText] = useState(props.edit ? props.edit.value : '');
+    // const [content, setContent] = useState('');
+
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        inputRef.current.focus();
+    });
+
+    const handleChangeText = e => {
+        setText(e.target.value);
+    };
+
+    // const handleChangeContent = e => {
+    //     setContent(e.target.value);
+    // };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        props.onSubmit({
+            id: Math.floor(Math.random() * 10000),
+            text: text,
+            // content: content,
+        });
+
+        setText('');
+        // setContent('');
+    };
+
+    return (
+        <div>
+            <form className="todo-form" onSubmit={handleSubmit}>
+
+                {props.edit ? ( <><input
+                        type="text"
+                        placeholder="Modifier la tâche"
+                        value={text}
+                        name="text"
+                        className="todo-input"
+                        onChange={handleChangeText}
+                        ref={inputRef}
+                    />
+
+                    {/*<input*/}
+                    {/*    type="text"*/}
+                    {/*    placeholder="Contenu de la tâche"*/}
+                    {/*    value={content}*/}
+                    {/*    name="content"*/}
+                    {/*    className="todo-input"*/}
+                    {/*    onChange={handleChangeContent}*/}
+                    {/*    ref={inputRef}*/}
+                    {/*/>*/}
+
+                    <button className="todo-button">Modifier la tâche</button> </>) :
+                    (<><input
+                    type="text"
+                    placeholder="Titre de la tâche"
+                    value={text}
+                    name="text"
+                    className="todo-input"
+                    onChange={handleChangeText}
+                    ref={inputRef}
+                    />
+                    {/*<input*/}
+                    {/*type="text"*/}
+                    {/*placeholder="Contenu de la tâche"*/}
+                    {/*value={content}*/}
+                    {/*name="content"*/}
+                    {/*className="todo-input"*/}
+                    {/*onChange={handleChangeContent}*/}
+                    {/*ref={inputRef}*/}
+                    {/*/>*/}
+
+                    <button className="todo-button">Ajouter une tâche</button> </>)
+                }
+
+
+            </form>
+        </div>
+    );
+}
+
+export default TodoForm;
